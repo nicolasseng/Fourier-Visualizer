770afe7c0de28eebc78d4041aec7bbe31169f164
nicolasseng <116186244+nicolasseng@users.noreply.github.com>
2025-06-04 19:03:27 +0200

    Add functionality for sin, cos, square wave

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,31 @@ import Plot from "react-plotly.js";
 
 function App() {
   const [data, setData] = useState(null);
+  const [func, setFunc] = useState("sin");
 
   useEffect(() => {
-    axios.get("http://localhost:8000/fft").then((res) => {
+    axios.get(`http://localhost:8000/fft?f=${func}&start=0&stop=10&points=100`).then((res) => {
       setData(res.data);
     });
-  }, []);
+  }, [func]);
+
 
   if (!data) return <div>Lade...</div>
   return (
     <div>
-      <h1>Fourier Demo</h1>
+      <div style={{ textAlign: "center", marginBottom: "1rem" }}>
+        <h1>Fourier Visualization</h1>
+        <select value={func} onChange={(e) => setFunc(e.target.value)}>
+          <option value="sin">sin(x)</option>
+          <option value="cos">cos(x)</option>
+          <option value="square">square wave</option>
+        </select>
+      </div>
       <Plot
         data={[
           { x: data.x, y: data.f, type: "scatter", name: "f(x)" },
         ]}
-        layout={{ title: "f(x) = sin(x)" }}
+        layout={{ title: `f(x) = ${func}(x)` }}
       />
       <Plot
         data={[
--- main.py
+++ main.py
@@ -9,15 +9,24 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
 @app.get("/fft")
-def compute_fft():
-    x = jnp.linspace(0, 10, 100)
-    y = jnp.sin(x)
+def compute_fft(f: str, start: float, stop: float, points: int):
+    x = jnp.linspace(start, stop, points)
+    if f == "sin":
+        y = jnp.sin(x)
+    elif f == "cos":
+        y = jnp.cos(x)
+    elif f == "square":
+        y = jnp.sign(jnp.sin(x))
+    else:
+        return {"Error": "Unknown Function"}
+
     y_fourier = jnp.fft.rfft(y)
     freqs = jnp.fft.rfftfreq(len(x), d=(x[1] - x[0]))
     return {
